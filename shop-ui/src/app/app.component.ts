d9e860f48047aa016193bac51c6674c1c0ab262b
prabhus0303 <prabhu.sundar94@gmail.com>
2025-02-22 19:29:45 +0530

    Angular base project

--- shop-ui/src/app/app.component.ts
+++ shop-ui/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from "./components/app-header/app-header.component";
+import { CategoryListComponent } from "./components/category-list/category-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppHeaderComponent, CategoryListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shop-ui';
+
+  imagePath = 'assets/img/electronics.png';
+
 }
